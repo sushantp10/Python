64a744ec8f7bafc3654aa7db7581e87a660d8258
sushantp10 <36946069+sushantp10@users.noreply.github.com>
2020-06-13 18:43:02 +0530

    Update and rename file_delete.py to Temp_Files_delete.py

--- Temp_Files_delete.py
+++ Temp_Files_delete.py
@@ -1,14 +1,16 @@
 import shutil, os, pprint
+import glob
 
 pprint.pprint(os.listdir("C:\\Users\\admin\\Desktop"))
 source="C:\\Users\\admin\\Desktop\\Source\\Example.txt"
 destination= "C:\\Users\\admin\\Desktop\\Dist\\"
 
-#copy_con=shutil.copyfile(source,destination)                 #To copy content from src
-#shutil.rmtree(destination)                                         #To delete  folder
+#copy_con=shutil.copyfile(source,destination)             #To copy content from source
+#shutil.rmtree(destination)                               #To delete  folder
 
 temp_folder = glob.glob('C:\\Users\\admin\\AppData\\Local\\Temp\\**\*.*', recursive=True)
 for file in temp_folder:
+    #Exception Handling
     try:
         os.remove(file)
     except OSError as e:
